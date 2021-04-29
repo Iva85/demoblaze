9e3ce5632072801ea707a84f25d11ddfa7f6527f
Iva85 <ivapetkovic85@gmail.com>
2021-04-29 14:30:44 +0200

    updated files

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -5,12 +5,7 @@ const data = require("../fixtures/data.json")
 describe("Registration test", () => {
     before (() => {
         cy.visit('/index.html')
-        //cy.get(locators.register.navRegister).click()
     })
-
-    /*it('Visit demoblaze page', () => {
-        cy.visit('/')
-    })*/
 
     it ("Visibility check", () => {
         cy.get('#signin2').should('be.visible')
@@ -19,9 +14,6 @@ describe("Registration test", () => {
     it ("Status code check", () => {
         cy.request('https://www.demoblaze.com/index.html').then((response) => {
             expect(response.status).to.eq(200)
-            /*expect(response.body).to.have.length(500)
-            expect(response).to.have.property('headers')
-            expect(response).to.have.property('duration')*/
         }) 
     })
 
